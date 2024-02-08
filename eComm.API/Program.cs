e5a674a28fe3932f15294a047d0c46f036ef4ad3
Florin <stoflorin22@gmail.com>
2024-02-08 22:34:13 +0200

    Add CORS

--- eComm.API/Program.cs
+++ eComm.API/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("http://localhost:5173", "http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
         });
 });
 builder.Services.AddSwaggerGen(options =>
@@ -94,6 +94,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
