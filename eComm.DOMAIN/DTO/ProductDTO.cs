dcc6690783d20fbed76210a4b0fcb6fc71ca49a9
Florin <stoflorin22@gmail.com>
2024-04-16 14:38:04 +0300

    add id to productDTO

--- eComm.DOMAIN/DTO/ProductDTO.cs
+++ eComm.DOMAIN/DTO/ProductDTO.cs
@@ -2,11 +2,13 @@
 {
     public class ProductDTO
     {
+        public int BookID { get; set; }
         public string ISBN { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Publisher { get; set; } = string.Empty;
         public string PublicationYear { get; set; } = string.Empty;
         public string ImageUrlL { get; set; } = string.Empty;
+        public int AverageRating { get; set; }
     }
 }
